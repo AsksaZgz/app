92fd91e4ed03f13a8106da9aac64951e3073119c
jesus.bernad <jesus.bernad@clearone.com>
2018-11-22 15:58:12 +0100

    typertask action bugs

--- office/typertask/typertask.action.cmd
+++ office/typertask/typertask.action.cmd
@@ -6,21 +6,22 @@
 	
 	
 :TYPERTASK_ACTION
-	if /i [%_typerTaskAction%]==[restart] 	call :TYPERTASK_ACTION_RESTART
-	if /i [%_typerTaskAction%]==[] 			call :TYPERTASK_ACTION_RUN
+	if /i [%_typerTaskAction%]==[restart] 	goto :TYPERTASK_ACTION_RESTART
+	if /i [%_typerTaskAction%]==[] 			goto :TYPERTASK_ACTION_RUN
 	goto :TYPERTASK_EXIT
 		
 
 :TYPERTASK_ACTION_RESTART
 	taskkill /F /IM typertask.exe
     call :TYPERTASK_ACTION_RUN
-	goto :eof
+	goto :TYPERTASK_EXIT
 
 :TYPERTASK_ACTION_RUN
     %CD%\typertask.exe
 	rem START "Typertask" %CD%\typertask.exe
     rem START "AYUDA" %CD%\typertask.exe
-	goto :eof
+	goto :TYPERTASK_EXIT
 		
 :TYPERTASK_EXIT
-	rem EXIT
+    echo TYPERTASK_EXIT
+	EXIT
